bfc7c4bc3a74b3477cc276df8c26fc966a7a8c8a
Nico <59927633+nicoestrada@users.noreply.github.com>
2024-02-03 17:46:28 -0600

    updated mrr to reflect confirmed payouts

--- cmd/mrr.go
+++ cmd/mrr.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"math"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -13,35 +14,37 @@ import (
 	"github.com/stripe/stripe-go/payout"
 )
 
+// iterates every payout to a running sum / 12
 func getMRR() {
 	//stored in env
 	if err := godotenv.Load(); err != nil {
 		fmt.Println("No .env file found")
 	}
 	stripe.Key = os.Getenv("STRIPE_API_KEY")
-
 	params := &stripe.PayoutListParams{
 		Status: stripe.String("paid"),
 	}
-
-	//prevents auto pagination
-	params.Single = true
-
+	params.Single = true //prevents auto pagination
 	params.Limit = stripe.Int64(100)
-
 	i := payout.List(params)
+	total := float64(0)
 	for i.Next() {
 		c := i.Payout()
 		f := float64(c.Amount) / float64(100)
-		fmt.Println("$", f, " - ", c.Status)
+		total += f
 	}
-
+	fmt.Println(
+		"\n\n Total payouts = $",
+		math.Floor(((total * 100) / 100)),
+		"\n\n MRR = $",
+		math.Floor(((total*100)/100)/12),
+	)
 }
 
 // mrrCmd represents the mrr command
 var mrrCmd = &cobra.Command{
 	Use:   "mrr",
-	Short: "Get your MRR stats",
+	Short: "Get your real MRR",
 	Long:  `This is a working cmd that fetches your most recent 100 payouts and statuses`,
 	Run: func(cmd *cobra.Command, args []string) {
 		getMRR()
@@ -49,16 +52,5 @@ var mrrCmd = &cobra.Command{
 }
 
 func init() {
-
 	rootCmd.AddCommand(mrrCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// mrrCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// mrrCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
